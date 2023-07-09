bbd45a5b6435535c9dddae426d1b4bbd6078334e
Abhishek R <37078073+abhishek14k@users.noreply.github.com>
2023-07-09 23:21:05 +0530

    Re-usable Navbar on remote

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Consulting from "./components/pages/Consulting";
+import Marketing from "./components/pages/Marketing";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
+import ContactUs from "./components/pages/ContactUs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/services" element={<Services />}></Route>
+          <Route path="/consulting" element={<Consulting />}></Route>
+          <Route path="/marketing" element={<Marketing />}></Route>
+          <Route path="/products" element={<Products />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route path="/contact-us" element={<ContactUs />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/pages/MenuItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const MenuItem = [
+  {
+    title: "Marketing",
+    path: "/marketing",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Consulting",
+    path: "/consulting",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Design",
+    path: "/design",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Development",
+    path: "/development",
+    cName: "dropdown-link",
+  },
+];
